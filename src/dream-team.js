93666c6b31e171cc103efff953da1ed74dacba18
shinkare90 <55100900+Shinkarev90@users.noreply.github.com>
2021-03-30 00:57:23 +0400

    feat: add count-cats and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !members.sort) {
+    return false;
+}
+let arr = members.map(el => {
+    if (typeof el === 'string') {
+        return el.trim();
+    }
+    return '';
+})
+let result = '';
+let array = arr.forEach(function(el) {
+    if (el && typeof el === 'string'){
+        result += el[0];
+    }
+})
+return result.toUpperCase().split('').sort().join('');
 };
